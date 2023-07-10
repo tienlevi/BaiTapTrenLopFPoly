c2e2297533666b987584e2ceb46895765fc8ad19
TiennLevii <nguyentrachtien2401@gmail.com>
2023-07-10 17:59:24 +0700

    QUanAo

--- Form/login.js
+++ Form/login.js
@@ -7,29 +7,42 @@ const username = document.querySelector(".username");
 const password = document.querySelector(".password");
 const actionFormUser = document.querySelector(".form-user form");
 
-var prevScroll = window.pageYOffset
+var prevScroll = window.pageYOffset;
 
 function onScroll() {
-    let currentScroll = window.pageYOffset
-    if (prevScroll > currentScroll) {
-        headerFirst.style.top = "0px";
-        headerSecond.style.top = "0px"
-    } else {
-        headerFirst.style.top = "-55px";
-        headerSecond.style.top = "-45px"
-    }
-    prevScroll = currentScroll;
+  let currentScroll = window.pageYOffset;
+  if (prevScroll > currentScroll) {
+    headerFirst.style.top = "0px";
+    headerSecond.style.top = "0px";
+  } else {
+    headerFirst.style.top = "-55px";
+    headerSecond.style.top = "-45px";
+  }
+  prevScroll = currentScroll;
 }
 
-window.addEventListener("scroll", onScroll)
+window.addEventListener("scroll", onScroll);
 
+const array = [
+  {
+    username: "tienntph33298@fpt.edu.vn",
+    password: "tien2004",
+  },
+  {
+    username: "tien@gmail.com",
+    password: "tien2401",
+  },
+];
 
 actionFormUser.addEventListener("submit", (e) => {
-
-    if (username.value == "tienntph33298@fpt.edu.vn" && password.value == "tien2004") {
-        actionFormUser.action = "/index.html"
-    } else {
-        falseUser.innerHTML = "Tài khoản hoặc mật khẩu không đúng";
-        e.preventDefault();
-    }
-})
+  if (
+    username.value == array[0].username &&
+    password.value == array[0].password
+  ) {
+    actionFormUser.action = "/Tài khoản/taikhoan.html";
+  } else {
+    falseUser.innerHTML = "Tài khoản hoặc mật khẩu không đúng";
+    e.preventDefault();
+  }
+});
+console.log(dataArray);
